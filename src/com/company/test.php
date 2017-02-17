361b91c485f65ed9a9dc21db7320c2b4d304bdc4
Kenneth Chinedu <kchinedu@mail.ru>
2017-02-17 13:36:07 +0300

    implement cyclomatic complexity

--- src/com/company/test.php
+++ src/com/company/test.php
@@ -1,61 +1,116 @@
 <?php
-private function _countCalls(PHP_Depend_Code_AbstractCallable $callable)
+global $DB_HOST, $DB_DB, $DB_USER, $DB_PASS, $DB_PORT;
+$DB_HOST	= 'localhost';
+$DB_DB		= 'database';
+$DB_USER	= 'username';
+$DB_PASS	= 'password';
+$DB_PORT	= 3306;
+
+function db_connect()
 {
-    $callT  = array(
-        \PDepend\Source\Tokenizer\Tokens::T_STRING,
-        \PDepend\Source\Tokenizer\Tokens::T_VARIABLE
-    );
-    $chainT = array(
-        \PDepend\Source\Tokenizer\Tokens::T_DOUBLE_COLON,
-        \PDepend\Source\Tokenizer\Tokens::T_OBJECT_OPERATOR,
-    );
+	global $DB_HOST, $DB_DB, $DB_USER, $DB_PASS;
 
-    $called = array();
+	if(!function_exists('mysql_pconnect'))
+		die('MySQL, a critical component for this site, is not accessible through PHP. Contact your system administrator or web host for more details.');
 
-    $tokens = $callable->getTokens();
-    $count  = count($tokens);
-    for ($i = 0; $i < $count; ++$i) {
-        // break on function body open
-        if ($tokens[$i]->type === \PDepend\Source\Tokenizer\Tokens::T_CURLY_BRACE_OPEN) {
-            break;
-        }
-    }
+	$_SESSION['db'] = @mysql_pconnect($DB_HOST, $DB_USER, $DB_PASS);
 
-    for (; $i < $count; ++$i) {
-        // Skip non parenthesis tokens
-        if ($tokens[$i]->type !== \PDepend\Source\Tokenizer\Tokens::T_PARENTHESIS_OPEN) {
-            continue;
-        }
-        // Skip first token
-        if (!isset($tokens[$i - 1]) || !in_array($tokens[$i - 1]->type, $callT)) {
-            continue;
-        }
-        // Count if no other token exists
-        if (!isset($tokens[$i - 2]) && !isset($called[$tokens[$i - 1]->image])) {
-            $called[$tokens[$i - 1]->image] = true;
-            ++$this->_calls;
-            continue;
-        } else if (in_array($tokens[$i - 2]->type, $chainT)) {
-            $identifier = $tokens[$i - 2]->image . $tokens[$i - 1]->image;
-            for ($j = $i - 3; $j >= 0; --$j) {
-                if (!in_array($tokens[$j]->type, $callT)
-                    && !in_array($tokens[$j]->type, $chainT)
-                ) {
-                    break;
-                }
-                $identifier = $tokens[$j]->image . $identifier;
-            }
+	if(!$_SESSION['db'])
+		die('Unable to establish a connection to the database. Contact your system administrator.');
 
-            if (!isset($called[$identifier])) {
-                $called[$identifier] = true;
-                ++$this->_calls;
-            }
-        } else if ($tokens[$i - 2]->type !== \PDepend\Source\Tokenizer\Tokens::T_NEW
-            && !isset($called[$tokens[$i - 1]->image])
-        ) {
-            $called[$tokens[$i - 1]->image] = true;
-            ++$this->_calls;
-        }
-    }
+	@mysql_select_db($DB_DB, $_SESSION['db']);
 }
-?>
+
+function db_backup()
+{
+	$filename = $_SERVER['HTTP_HOST'] . '_' . date('YmdHi') . '.sql.gz';
+	$file = '/tmp/'.$filename;
+
+	passthru("mysqldump --user=$DB_USER --password=$DB_PASS --host=$DB_HOST $DB_DB | gzip &gt; $file");
+
+	$file_part = explode('/', $file);
+
+	header('Content-Description: File Transfer');
+	header('Content-Type: application/octet-stream');
+	header('Content-Length: ' . (string)(filesize($file)));
+	header('Content-Disposition: attachment; filename=' . $file_part[(count($file_part) - 1)]);
+
+	readfile($file);
+}
+
+function db_escape($var)
+{
+	return @mysql_escape_string($var);
+}
+
+function db_write($q)
+{
+	$rs = db_query($q);
+	return $rs ? 1 : 0;
+}
+
+function db_query($q)
+{
+	if(!isset($_SESSION['db']) || !$_SESSION['db'])
+		db_connect();
+
+	if(!@is_resource($_SESSION['db']))
+		db_error('Not connected to a database. Cannot execute query.');
+
+	if(isset($_SESSION['queries']))
+		$_SESSION['queries'][] = $q;
+
+	if(!$return = @mysql_query($q, $_SESSION['db']))
+		db_error('Could not execute the following query&lt;br /&gt;' . "\n" . '&lt;pre id="query"&gt;' . $q . '&lt;/pre&gt;' . mysql_error($_SESSION['db']));
+
+	return $return;
+}
+
+function db_fetch_all($q, $fetch = DB_ASSOC)
+{
+	$rs = db_query($q);
+
+	switch($fetch)
+	{
+		case DB_ROW:
+			$function = 'mysql_fetch_row';
+			break;
+		case DB_ARRAY:
+			$function = 'mysql_fetch_array';
+			break;
+		case DB_ASSOC:
+		default:
+			$function = 'mysql_fetch_assoc';
+			break;
+	}
+
+	$return = array();
+
+	while($row = @$function($rs))
+		$return[] = $row;
+
+	return $return;
+}
+
+function db_fetch_one($q)
+{
+	$rs = db_query($q);
+	$row = @mysql_fetch_assoc($rs);
+
+	return count($row) ? $row : 0;
+}
+
+function db_count($q)
+{
+	$rs = db_query($q);
+	$row = @mysql_fetch_row($rs);
+
+	return count($row) ? $row[0] : 0;
+}
+
+function db_error($err)
+{
+	// included a file that would read $err inside of the site's content
+	exit;
+}
+?>;
