01e9f23a37c3d583a3deb7de67d1af7cb277a7d7
Olga <minakovamail@rocketmail.com>
2017-02-17 13:33:54 +0300

    number of methods count number of comments count

--- src/com/company/test.php
+++ src/com/company/test.php
@@ -1,61 +1,57 @@
 <?php
-private function _countCalls(PHP_Depend_Code_AbstractCallable $callable)
-{
-    $callT  = array(
-        \PDepend\Source\Tokenizer\Tokens::T_STRING,
-        \PDepend\Source\Tokenizer\Tokens::T_VARIABLE
-    );
-    $chainT = array(
-        \PDepend\Source\Tokenizer\Tokens::T_DOUBLE_COLON,
-        \PDepend\Source\Tokenizer\Tokens::T_OBJECT_OPERATOR,
-    );
+class Musician {
 
-    $called = array();
+    protected $name;
 
-    $tokens = $callable->getTokens();
-    $count  = count($tokens);
-    for ($i = 0; $i < $count; ++$i) {
-        // break on function body open
-        if ($tokens[$i]->type === \PDepend\Source\Tokenizer\Tokens::T_CURLY_BRACE_OPEN) {
-            break;
-        }
+    public function __construct($name)
+    {
+        $this->name = $name;
     }
 
-    for (; $i < $count; ++$i) {
-        // Skip non parenthesis tokens
-        if ($tokens[$i]->type !== \PDepend\Source\Tokenizer\Tokens::T_PARENTHESIS_OPEN) {
-            continue;
-        }
-        // Skip first token
-        if (!isset($tokens[$i - 1]) || !in_array($tokens[$i - 1]->type, $callT)) {
-            continue;
-        }
-        // Count if no other token exists
-        if (!isset($tokens[$i - 2]) && !isset($called[$tokens[$i - 1]->image])) {
-            $called[$tokens[$i - 1]->image] = true;
-            ++$this->_calls;
-            continue;
-        } else if (in_array($tokens[$i - 2]->type, $chainT)) {
-            $identifier = $tokens[$i - 2]->image . $tokens[$i - 1]->image;
-            for ($j = $i - 3; $j >= 0; --$j) {
-                if (!in_array($tokens[$j]->type, $callT)
-                    && !in_array($tokens[$j]->type, $chainT)
-                ) {
-                    break;
-                }
-                $identifier = $tokens[$j]->image . $identifier;
-            }
+    public function getName()
+    {
+        return $this->name . "<br>";
+    }
 
-            if (!isset($called[$identifier])) {
-                $called[$identifier] = true;
-                ++$this->_calls;
-            }
-        } else if ($tokens[$i - 2]->type !== \PDepend\Source\Tokenizer\Tokens::T_NEW
-            && !isset($called[$tokens[$i - 1]->image])
-        ) {
-            $called[$tokens[$i - 1]->image] = true;
-            ++$this->_calls;
-        }
+    final public function ballin()
+    {
+        return $this->name . " is the greatest ever!<br>"; /*
+                                                             This is a multiple-lines comment block
+                                                             that spans over multiple
+                                                             lines
+                                                            */
+    }
+
+}
+
+class Guitarist extends Musician {
+
+#Single-line comment 3
+
+    public function getName()
+    {
+        return "My name is " . $this->name . "<br>";
+    }
+
+}
+
+$musician = new Musician('Slash');
+$guitarist = new Guitarist('Stevie Ray Vaughan');
+echo $musician->getName();
+echo $musician->ballin();
+echo $guitarist->getName();
+echo $guitarist->ballin();
+?>
+
+
+<?php
+class Foo {
+    public static function aStaticMethod() {
+        // Single-line comment 1
     }
 }
+
+Foo::aStaticMethod();
+$classname = 'Foo';
+$classname::aStaticMethod(); // Single-line comment 2
 ?>
